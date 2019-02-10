397094f4b459242a4d28c497d7f50248c687d3fc
G3org3B <vbt.george@gmail.com>
2019-02-11 00:30:24 +0100

    commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.sass('src/sass/app.scss', 'public/css/app.css');
+mix.js('src/js/app.js').sass('src/sass/app.scss', 'public/css/app.css');
 
 // Full API
 // mix.js(src, output);
