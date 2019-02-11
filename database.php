bec267e19e967a04a263506ca812f72c12e4974f
G3org3B <vbt.george@gmail.com>
2019-02-11 01:28:40 +0100

    commit

--- database.php
+++ database.php
@@ -5,14 +5,17 @@
     $conn = new Mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error)
+
     {
         echo 'Si è verificato un errore: ' . $conn->connect_error;
     }
+
 
     $sql = 'SELECT * FROM `stanze`';
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0)
+
     {
         $rooms = [];
 
