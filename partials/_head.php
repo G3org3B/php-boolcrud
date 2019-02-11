77395287139d78bad7f66b38dc7bdd15d2be9bf2
G3org3B <vbt.george@gmail.com>
2019-02-11 01:24:38 +0100

    commit

---
+++ partials/_head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>La prima CRUD</title>
+        <link rel="stylesheet" href="http://localhost/Boolean/php-crud/public/css/app.css">
+    </head>
+    <body>
---
+++ partials/_navbar.php
@@ -0,0 +1,25 @@
+<div class="b-navbar">
+    <div class="container">
+        <div class="b-navbar-left">
+            <a href="http://localhost/Boolean/php-crud/index.php">
+                <img src="http://boolean.careers/images/common/logo.png" alt="">
+            </a>
+        </div>
+        <div class="b-navbar-right">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">About us</a>
+                </li>
+                <li>
+                    <a href="#">How it Works</a>
+                </li>
+                <li>
+                    <a href="#">Sign In</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
